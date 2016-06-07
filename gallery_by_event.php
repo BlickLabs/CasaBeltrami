060bbbebe2058236085b6f25c2f207772235358b
Alan Sanchez <sanchezpineda03@gmail.com>
2016-06-07 00:53:03 -0500

    Gallery by events.

--- gallery_by_event.php
+++ gallery_by_event.php
@@ -162,7 +162,7 @@
                                                     <?php
                                                         include "config.php";
                                                         error_reporting(E_ALL);
-                                                        $res2 = $mysqli2->query("SELECT DISTINCT c.id_content,c.tittle,c.route,c.description FROM content_party_room AS cpr LEFT JOIN content AS c ON c.id_content = cpr.id_content  WHERE cpr.id_event ='" . $id_pr . "'");
+                                                        $res2 = $mysqli2->query("SELECT DISTINCT c.id_content,c.tittle,c.route,c.description FROM content_event AS ce LEFT JOIN content AS c ON c.id_content = ce.id_content  WHERE ce.id_event ='" . $id_pr . "'");
                                                          $mysqli2->close();
                                                          while ($row2 = $res2->fetch_assoc()){
                                                              $path= 'php/album/' . $row2['route'];
